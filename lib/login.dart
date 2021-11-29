7b9eb4fbab08712377793218104359b535994cd0
chabdullah333 <93539138+chabdullah333@users.noreply.github.com>
2021-11-29 22:52:35 +0500

    attendance

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:secondapp/dashboard.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class loginpage extends StatefulWidget {
   const loginpage({Key? key}) : super(key: key);
@@ -9,88 +11,139 @@ class loginpage extends StatefulWidget {
 }
 
 class _loginpageState extends State<loginpage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  late String username, password;
+  late bool error, sending, success;
+  late String msg;
+  late String role;
+  String url = "http://192.168.43.166:5000/testingtesting";
+  @override
+  void initState() {
+    error = false;
+    sending = false;
+    success = false;
+    super.initState();
+  }
+
+  Future<void> verifylogin() async {
+    var res = await http.post(Uri.parse(url), body: {
+      'username': usernameController.text,
+      'password': passwordController.text,
+      //'Role': roleController.text,
+    });
+    if (res.statusCode == 200) {
+      print(res.body);
+      var data = json.decode(res.body);
+      print(data);
+      if (data != null) {
+        setState(() {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => dashbord()));
+        });
+      }
+      if (data["error"]) {
+        setState(() {
+          sending = false;
+          error = true;
+          msg = data["message"];
+        });
+      } else {
+        usernameController.text = '';
+        passwordController.text = '';
+        setState(() {
+          sending = false;
+          success = true;
+        });
+      }
+    } else {
+      setState(() {
+        error = true;
+        msg = "Error during sending data";
+        sending = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: ListView(
-            padding: EdgeInsets.symmetric(horizontal: 58.0),
-            children: [
-              Column(
-                children: [
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  Image.asset('images/loginimg.jpeg'),
-                  Text(
-                    'Welcome to login',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              TextField(
-                cursorColor: Colors.red,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    decorationColor: Colors.white,
-                  ),
-                  filled: true,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 58.0),
+          children: [
+            Column(
+              children: [
+                const SizedBox(
+                  height: 60,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                obscureText: true,
-                cursorColor: Colors.red,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    decorationColor: Colors.white,
-                  ),
-                  filled: true,
+                Image.asset('images/login.png'),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            TextField(
+              controller: usernameController,
+              cursorColor: Colors.red,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  decorationColor: Colors.white,
                 ),
+                filled: true,
               ),
-              SizedBox(
-                height: 20,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              cursorColor: Colors.red,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  decorationColor: Colors.white,
+                ),
+                filled: true,
               ),
-              Column(
-                children: [
-                  ButtonTheme(
-                    height: 40,
-                    disabledColor: Colors.blue,
-                    child: RaisedButton(
-                      onPressed: () {
-                        setState(() {
-                          Text('login successfully');
-                        });
-                      },
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                ButtonTheme(
+                  height: 40,
+                  disabledColor: Colors.blue,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // verifylogin();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => dashbord()),
+                      );
+
+                      //setState(() {});
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:secondapp/courses.dart';
+import 'package:secondapp/dashboard.dart';
 import 'package:secondapp/login.dart';
-import 'package:secondapp/login.dart';
+import 'package:secondapp/verifyattendance.dart';
 
 void main() {
-  runApp(loginpage());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.redAccent,
-        appBar: AppBar(
-          title: Text('hey'),
-          centerTitle: true,
-          backgroundColor: Colors.deepOrangeAccent,
-        ),
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                height: 100.0,
-                color: Colors.white,
-                child: Text('Container 1'),
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              Container(
-                height: 200.0,
-                color: Colors.cyan,
-                child: Text('Container 2'),
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              Container(
-                height: 100.0,
-                color: Colors.amber,
-                child: Text('Container 3'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    home: dashbord(),
+  ));
 }
